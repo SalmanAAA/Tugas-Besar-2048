612f60a1aecdc773c6232fd351d865a4e689b678
SalmanAAA <bdod307@gmail.com>
2024-11-13 08:00:33 +0700

    committest2

--- Main.c
+++ Main.c
@@ -5,6 +5,9 @@
 int main (){
   printf("============2048============\n");
   printf("|                           |\n");
+  printf("|    welcome to our game    |\n");
   printf("|                           |\n");
+  printf("|                           |\n");
+  printf("============================|\n");
   return 0;
 }
