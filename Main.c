865e84c6973d85beb75c2a84bfeb4b414460861e
SalmanAAA <bdod307@gmail.com>
2024-11-16 19:30:08 +0700

    add modul papan permainan

--- Main.c
+++ Main.c
@@ -2,13 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define SIZE 4 //Define SIZE Array 4x4
+
+void printPapan(int papan[SIZE][SIZE]); //Modul pertama untuk program menampilkan board
+
 int main (){
-  printf("============2048============\n");
-  printf("|                           |\n");
-  printf("|    welcome to our game    |\n");
-  printf("|                           |\n");
-  printf("============================|\n");
-  //scanf();
-  //test
+
+  int papan[SIZE][SIZE] = {0};
+  printPapan(papan);
+
   return 0;
+}
+
+void printPapan(int papan[SIZE][SIZE]){
+  for (int i=0;i<SIZE;i++){
+    for(int j=1;j<=SIZE;j++){
+      if (papan[i][j]==0){
+        printf("|\t"); //menampilkan board 2 Dimensi.
+      }else{
+        printf("%d\t",papan[i][j]);
+      }
+    }
+    printf("\n");
+  }
+  printf("\n");
 }
