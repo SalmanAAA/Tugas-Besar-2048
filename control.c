4bb2e10e0eb2341d6cb4db17559224d4b4439430
SalmanAAA <bdod307@gmail.com>
2024-11-25 14:07:13 +0700

    bawah

--- control.c
+++ control.c
@@ -24,3 +24,21 @@ void atas(){
         }
     }
 }
+void bawah(){
+    for (int j = 0; j < SIZE; j++){
+        for (int i = 1; i < SIZE; i--){
+            if (papan[i][j] != 0){
+                int k = i; //var k buat nandain pergerakan di baris-1 (keatas)
+                while (k > 0 && papan[k+1][j] == 0){
+                    papan[k+1][j] = papan[k][j];
+                    papan[k][j] = 0;
+                    k++; //k-- keatas lagi
+                }
+                if (k > 0 && papan[k+1][j] == papan{k}[j]){
+                    papan[k+1][j] *= 2;
+                    papan[k][j] = 0;
+                }
+            }
+        }
+    }
+}
