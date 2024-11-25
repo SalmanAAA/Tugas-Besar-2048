0433097e0f1483489e9ab4e787a8b1ae208b7ac3
vermilion10 <fathinyassarahmaan@gmail.com>
2024-11-25 14:25:08 +0700

    mff keliru variabel

--- control.c
+++ control.c
@@ -29,12 +29,12 @@ void bawah(){
         for (int i = SIZE - 2; i >= 0; i--){
             if (papan[i][j] != 0){
                 int k = i; //var k buat nandain pergerakan di baris+1 (kebawah)
-                while (k < SIZE - 1 && board[k+1][j] == 0){
+                while (k < SIZE - 1 && papan[k+1][j] == 0){
                     papan[k+1][j] = papan[k][j];
                     papan[k][j] = 0;
                     k++; //k++ kebawah lagi
                 }
-                if (k < SIZE - 1 && board[k+1][j] == board[k][j]){
+                if (k < SIZE - 1 && papan[k+1][j] == papan[k][j]){
                     papan[k+1][j] *= 2;
                     papan[k][j] = 0;
                 }
