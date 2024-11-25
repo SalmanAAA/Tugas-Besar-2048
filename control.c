6cda267d3c07c554963b8d58b0ba77954df8884b
vermilion10 <fathinyassarahmaan@gmail.com>
2024-11-25 13:52:09 +0700

    nambah modul control keatas

--- control.c
+++ control.c
@@ -1,4 +1,26 @@
 #include "control.h"
+#include "papan.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+extern int papan[SIZE][SIZE]
+
+void atas(){
+    for (int j = 0; j < SIZE; j++){
+        for (int i = 1; i < SIZE; i++){
+            if (papan[i][j] != 0){
+                int k = i; //fungsi k buat nandain pergerakan di baris-1 (keatas)
+                while (k > 0 && papan[k-1][j] == 0){
+                    papan[k-1][j] = papan[k][j];
+                    papan[k][j] = 0;
+                    k--;
+                }
+                if (k > 0 && papan[k-1][j] == papan{k}[j]){
+                    papan[k-1][j] *= 2;
+                    papan[k][j] = 0;
+                }
+            }
+        }
+    }
+}
